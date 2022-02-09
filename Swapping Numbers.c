04a3248441aceff43ed96d0875351a49ce969ea1
Arpitp04 <99361053+Arpitp04@users.noreply.github.com>
2022-02-10 01:27:46 +0530

    Add files via upload
    Swapping Two Numbers without a third variable

---
+++ Swapping Numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int x,y;
+    printf("Enter value of x: ");
+    scanf("%d",&x);
+    printf("Enter value of y: ");
+    scanf("%d",&y);
+    printf("X = %d and Y = %d \n\n",x,y);
+	x = x + y; 
+    y = x - y; 
+    x = x - y; 
+    printf("After Swapping the numbers x = %d, y = %d",x,y);
+    return(0);
+}
