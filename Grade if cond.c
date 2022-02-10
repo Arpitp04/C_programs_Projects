4d3aedf61cb9360fa4cfc63e6c9c144efdae20cc
Arpitp04 <99361053+Arpitp04@users.noreply.github.com>
2022-02-10 10:05:07 +0530

    Update Grade if cond.c

--- Grade if cond.c
+++ Grade if cond.c
@@ -7,26 +7,26 @@ int main()
     if (marks>=85 && marks<=100)
     {
     	printf("\n GRADE A");
-	}
-	else if (marks>=70 && marks<=84)
+    }
+    else if (marks>=70 && marks<=84)
     {
     	printf("\n GRADE B");
-	}
-	else if (marks>=55 && marks<=69)
+    }
+    else if (marks>=55 && marks<=69)
     {
     	printf("\n GRADE C");
-	}
-	else if (marks>=40 && marks<=54)
+    }
+    else if (marks>=40 && marks<=54)
     {
     	printf("\n GRADE D");
-	}
-	else if (marks<40)
+    }
+    else if (marks<40)
     {
     	printf("\n GRADE F");
-	}
-	else 
-	{
-		printf("Enter marks out of 100 please");
-	}
+    }
+    else 
+    {
+	printf("Enter marks out of 100 please");
+    }
     return(0);
 }
